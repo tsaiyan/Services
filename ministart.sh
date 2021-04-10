d8a9d3a6a6537e75b06bc50c497557a9f990638d
tsaiyan <tsaiyan21@yandex.ru>
2021-04-10 16:27:57 +0300

    2 services runs ok

--- ministart.sh
+++ ministart.sh
@@ -2,4 +2,4 @@ minikube stop
 minikube delete
 VBoxManage hostonlyif remove vboxnet0
 minikube start --vm-driver=virtualbox --cpus=4 --memory=4G --disk-size=50G
-sh run.sh
+
--- nominisetup.sh
+++ nominisetup.sh
@@ -1,26 +1,21 @@
 RED= `tput setaf 2`
 NC='\033[0m'
 
-minikube stop
-minikube delete
-VBoxManage hostonlyif remove vboxnet0
-minikube start --vm-driver=virtualbox --cpus=4 --memory=4G --disk-size=50G
-eval $(minikube docker-env)
 minikube addons enable metrics-server
 minikube addons enable metallb
 echo "${RED}\nLET GO RUN SERVICES \n\n${NC}"
 #minikube stop
-kubectl delete --all pods
 #minikube delete
 #echo "MINICUBE START"
 #minikube start --vm-driver=docker
-docker rm nginx_image
 echo "DOCKER BUILD"
-docker build -t nginx_image ./srcs/nginx/
+eval $(minikube docker-env)
 docker build -t php_image ./srcs/phpmyadmin/
+eval $(minikube docker-env)
+docker build -t nginx_image ./srcs/nginx/
 #kubectl delete --all pods
-kubectl apply -f srcs/nginx/nginx.yaml
 kubectl apply -f srcs/configmap.yaml
+kubectl apply -f srcs/nginx/nginx.yaml
 kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
 minikube addons list
 kubectl get pods
