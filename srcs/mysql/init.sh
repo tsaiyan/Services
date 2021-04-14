ce9ba09ff595aa72926448f135678dd34f0e1ba5
tsaiyan <tsaiyan21@yandex.ru>
2021-04-14 20:59:58 +0300

    mysql can be seen from outside, but bd is wrong

--- srcs/mysql/init.sh
+++ srcs/mysql/init.sh
@@ -12,11 +12,13 @@
 
 #!/bin/sh
 mariadb-install-db --user=root
-mysqld --user=root & sleep 5
+mysqld --user=root & sleep 10
 #mysql -u root -p --skip-password
+#mysql --user=root --execute="CREATE DATABASE wordpress;"
 echo "CREATE DATABASE wordpress;" | mysql -u root -p --skip-password
-echo "CREATE USER 'wp-admin' IDENTIFIED BY 'password';" | mysql -u root -p --skip-password
-echo "GRANT ALL PRIVILEGES ON wordpress.* TO wp-admin;" | mysql -u root -p --skip-password
+mysql --user=root wordpress < wordpress.sql
+echo "CREATE USER 'wp_admin' IDENTIFIED BY 'password';" | mysql -u root -p --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO wp_admin;" | mysql -u root -p --skip-password
 echo "FLUSH PRIVILEGES;" | mysql -u root -p --skip-password
 #mysqld --user=root --execute= "CREATE DATABASE wordpress;"
 #mysqld --user=root --execute= "CREATE USER 'admin' IDENTIFIED BY 'password';"
@@ -26,6 +28,6 @@ echo "FLUSH PRIVILEGES;" | mysql -u root -p --skip-password
 #echo "CREATE USER 'admin' IDENTIFIED BY 'password';" | mysqld -u root -p --skip-password
 #echo "GRANT ALL PRIVILEGES ON wordpress.* TO admin;" | mysqld -u root -p --skip-password
 #echo "FLUSH PRIVILEGES;" | mysqld -u root -p --skip-password
-sh
-#/usr/bin/supervisord -c /etc/supervisor.conf
+#sh
+/usr/bin/supervisord -c /etc/supervisord.conf
 
