6103c55e6f37858cee9265fc322a2ff58e1e2e5b
tsaiyan <tsaiyan21@yandex.ru>
2021-04-16 17:03:17 +0300

    I can connect to ftps but via k3nt, not from just docker

--- nominisetup.sh
+++ nominisetup.sh
@@ -14,10 +14,12 @@ docker build -t nginx_image ./srcs/nginx/
 docker build -t php_image ./srcs/phpmyadmin/
 docker build -t wordpress_image ./srcs/wordpress/
 docker build -t mysql_image ./srcs/mysql/
+docker build -t ftps_image ./srcs/mysql/
 #kubectl delete --all pods
 kubectl apply -f srcs/configmap.yaml
 kubectl apply -f srcs/nginx/nginx.yaml
 kubectl apply -f srcs/mysql/mysql.yaml
+kubectl apply -f srcs/mysql/ftps.yaml
 kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
 kubectl apply -f srcs/wordpress/wordpress.yaml
 minikube addons list
