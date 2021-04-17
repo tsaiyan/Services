3b7fc5bd44a499c1ecfe04bec73064011a4db2d6
tsaiyan <tsaiyan21@yandex.ru>
2021-04-17 20:12:23 +0300

    grafana is upped '

--- nominisetup.sh
+++ nominisetup.sh
@@ -15,6 +15,7 @@ docker build -t php_image ./srcs/phpmyadmin/
 docker build -t wordpress_image ./srcs/wordpress/
 docker build -t mysql_image ./srcs/mysql/
 docker build -t ftps_image ./srcs/ftps/
+docker build -t grafana_image ./srcs/grafana/
 #kubectl delete --all pods
 kubectl apply -f srcs/configmap.yaml
 kubectl apply -f srcs/nginx/nginx.yaml
@@ -22,6 +23,7 @@ kubectl apply -f srcs/mysql/mysql.yaml
 kubectl apply -f srcs/ftps/ftps.yaml
 kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
 kubectl apply -f srcs/wordpress/wordpress.yaml
+kubectl apply -f srcs/grafana/grafana.yaml
 kubectl apply -f srcs/ftps/ftps.yaml
 minikube addons list
 kubectl get pods
